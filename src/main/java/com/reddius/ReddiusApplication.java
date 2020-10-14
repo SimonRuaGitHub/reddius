68b428e194cd660ec2f826eba3ad70371e8aa1e8
SimonRua <simon.ruavargas.contractor@bydeluxe.com>
2020-10-14 09:08:28 -0500

    adding swagger configuration bean

--- src/main/java/com/reddius/ReddiusApplication.java
+++ src/main/java/com/reddius/ReddiusApplication.java
@@ -3,10 +3,14 @@ package com.reddius;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.reddius.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class ReddiusApplication {
 
 	public static void main(String[] args) {
